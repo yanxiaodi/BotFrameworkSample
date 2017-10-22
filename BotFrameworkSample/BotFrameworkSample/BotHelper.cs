79dc7d6ee6e0bdb8576b6ec72cff16d4c454a515
Yan Xiaodi <yan_xiaodi@hotmail.com>
2017-10-22 14:25:30 +0800

    Create a chat bot sample.

---
+++ BotFrameworkSample/BotFrameworkSample/BotHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BotFrameworkSample
+{
+    public static class BotHelper
+    {
+        public static async Task SendResponse(string input, IDialogContext context)
+        {
+            if (input.ToLower().Contains("hi"))
+            {
+                await context.PostAsync($"Please input your package traker number.");
+            }
+            else if (input == "EMS1234567890")
+            {
+                await context.PostAsync($"Your package is still in auditing process. Please wait a moment and recheck later.");
+            }
+            else if (input == "EMS1234567891")
+            {
+                await context.PostAsync($"You have paid the tariff for your package. Please contact your shipper company.");
+            }
+            else if (input.StartsWith("EMS") && input.Length == 13)
+            {
+                await context.PostAsync($"You need to pay the tariff for your package: RMB150. Please choose your payment system: 1. Wechat Pay; 2. Alipay; 3. Later");
+            }
+            else if (input == "1")
+            {
+                await context.PostAsync($"You paid the tariff by Wechat Pay. Thanks for your collaboration! Please contact your shipper company to get your package.");
+            }
+            else if (input == "2")
+            {
+                await context.PostAsync($"You paid the tariff by Alipay. Thanks for your collaboration! Please contact your shipper company to get your package.");
+            }
+            else if (input == "3")
+            {
+                await context.PostAsync($"Thanks! You can pay the tariff later.");
+            }
+            else
+            {
+                await context.PostAsync($"I do not what you are saying. Please input your tracker number.");
+            }
+        }
+    }
+}
--- BotFrameworkSample/BotFrameworkSample/Dialogs/RootDialog.cs
+++ BotFrameworkSample/BotFrameworkSample/Dialogs/RootDialog.cs
@@ -19,11 +19,18 @@ namespace BotFrameworkSample.Dialogs
         {
             var activity = await result as Activity;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            int number = 0;
+            int.TryParse(activity.Text.Trim(), out number);
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            string input = activity.Text.Trim();
+
+            await BotHelper.SendResponse(input, context);
+
+            //// calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            //// return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
         }
